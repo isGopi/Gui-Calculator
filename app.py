18c54ebc347c5e130c8e3a2a0f2d3e85075e00b0
Gopi <73015341+Gurpreet707@users.noreply.github.com>
2023-04-17 18:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ class Calculator(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Calculator")
+        self.configure(bg='black')
+        self.maxsize(500, 600)
+        self.geometry("+%d+%d" % ((self.winfo_screenwidth() - 500) // 2, (self.winfo_screenheight() - 600) // 2))
 
+        # Configure the grid layout to resize columns and rows
+        self.grid_columnconfigure(0, weight=1)
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
         # Entry widget to display the numbers and results
         self.display = tk.Entry(self, width=25, font=('Arial', 20))
         self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
@@ -32,7 +39,9 @@ class Calculator(tk.Tk):
         # Evaluate button
         evaluate_button = tk.Button(self, text='=', width=5, height=2, font=('Arial', 15), command=self.evaluate)
         evaluate_button.grid(row=5, column=2, padx=5, pady=5)
-
+        # Make buttons grow when window is resized
+        for i in range(4):
+            self.grid_columnconfigure(i, weight=1)
     def button_click(self, event):
         button_text = event.widget['text']
         self.display.insert(tk.END, button_text)
